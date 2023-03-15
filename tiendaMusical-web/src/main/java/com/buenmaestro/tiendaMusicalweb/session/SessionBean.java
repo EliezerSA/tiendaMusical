9671d8eb594eeba4688fe0b3c727b8f71c8494ab
EliezerSA <eliezersantiago-100@outlook.com>
2023-03-14 20:39:34 -0600

    Nombre del Peoyecto: TiendaMusical. Descripcion: Se suben cambios de integracion de login, redireccionamiento e inicio de sesion con los datos de consulta en BD.
    Adicionalmente se agregan las pantallas de cliente, admin, dashboard y
    login para  su rediseño.

---
+++ tiendaMusical-web/src/main/java/com/buenmaestro/tiendaMusicalweb/session/SessionBean.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.buenmaestro.tiendaMusicalweb.session;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.buenmaestro.tiendaMusicalentities.entities.Persona;
+
+/**
+ * @author Eliezer
+ * Clase que mantendra la informacion en la sesion del usuario
+ *
+ */
+@ManagedBean
+@SessionScoped
+public class SessionBean {
+	
+	private Persona persona;
+	
+	public void init() {
+		System.out.println("Creando sesion......");
+	}
+
+	/**
+	 * @return the persona
+	 */
+	public Persona getPersona() {
+		return persona;
+	}
+
+	/**
+	 * @param persona the persona to set
+	 */
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+	
+
+}
